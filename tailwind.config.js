4c82d11ee4e4ee76fa4bc75e6aeb128d5141c31f
kimkwangkuk <kim12795@gmail.com>
2024-11-12 00:41:30 +0900

    애플 스타일 디자인으로 UI 개선

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // src 폴더 내의 모든 JS/JSX/TS/TSX 파일
+    "./public/index.html",         // public 폴더의 HTML 파일
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Apple SD Gothic Neo',
+          'Pretendard',
+          'system-ui',
+          'sans-serif'
+        ],
+      },
+    },
   },
   plugins: [],
 }
