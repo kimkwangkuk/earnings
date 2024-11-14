e9cdf9c9c5965b8150077bc68434dce95c5b0067
kimkwangkuk <kim12795@gmail.com>
2024-11-14 23:07:19 +0900

    CSS와 Tailwind 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // src 폴더 내의 모든 JS/JSX/TS/TSX 파일
-    "./public/index.html",         // public 폴더의 HTML 파일
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: [
+          'Pretendard',
           '-apple-system',
           'BlinkMacSystemFont',
           'Apple SD Gothic Neo',
-          'Pretendard',
           'system-ui',
           'sans-serif'
         ],
